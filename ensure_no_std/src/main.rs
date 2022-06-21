6cafc7352a521125e5827bb5f725e62f2f879725
Warlock <internalmike@gmail.com>
2022-06-21 15:21:15 +0300

    ensure_no_std

--- ensure_no_std/src/main.rs
+++ ensure_no_std/src/main.rs
@@ -1,49 +1,22 @@
-#![feature(default_alloc_error_handler)]
 #![feature(start)]
 
 #![deny(warnings)]
 
 #![no_std]
 
-use core::alloc::Layout;
-use core::panic::PanicInfo;
-#[cfg(not(windows))]
-use libc::exit;
-use libc_alloc::LibcAlloc;
-use tuifw_window::{RenderPort, WindowTree, Window};
-#[cfg(windows)]
-use winapi::shared::minwindef::UINT;
-#[cfg(windows)]
-use winapi::um::processthreadsapi::ExitProcess;
-
 #[cfg(windows)]
 #[link(name="msvcrt")]
 extern { }
 
-#[global_allocator]
-static ALLOCATOR: LibcAlloc = LibcAlloc;
-
-#[cfg(windows)]
-unsafe fn exit(code: UINT) -> ! {
-    ExitProcess(code);
-    loop { }
-}
-
 #[panic_handler]
-pub extern fn panic(_info: &PanicInfo) -> ! {
-    unsafe { exit(99) }
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    exit_no_std::exit(99)
 }
 
-#[no_mangle]
-pub fn rust_oom(_layout: Layout) -> ! {
-    unsafe { exit(98) }
-}
-
-fn render<State: ?Sized>(_: &WindowTree<State>, _: Option<Window>, _: &mut RenderPort, _: &mut State) { }
+use int_vec_2d::*;
 
 #[start]
 pub fn main(_argc: isize, _argv: *const *const u8) -> isize {
-    let screen = unsafe { tuifw_screen::init() }.unwrap();
-    let _windows = WindowTree::<()>::new(screen, render);
+    assert_eq!(Point { x: 1, y: 2}.offset(Vector { x: 5, y: -10 }), Point { x: 6, y: -8 });
     0
 }
